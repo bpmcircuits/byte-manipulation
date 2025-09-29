a83ed056293a00e2356595263f7593f0d1bc62a4
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-09-29 23:07:50 +0200

    deleted unused imports

--- src/main/java/com/bpm/kodilla/bytecode/reflection/controller/StudentController.java
+++ src/main/java/com/bpm/kodilla/bytecode/reflection/controller/StudentController.java
@@ -6,15 +6,11 @@ import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Validated
